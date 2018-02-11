94fb2a7ce1e50a99543f2506192cc3cd795ed005
Ron Shaiko <Ron.Shaiko@amdocs.com>
2018-02-11 17:16:28 +0200

    added condition

--- wc.py
+++ wc.py
@@ -1,14 +1,14 @@
 def get_file(filename):
     try:
         file = open(filename, "r")  # opens file with name of "alice30.txt"
-        return file
+        return file, None
     except Exception:
-        print "sorry, no file existed with the name " + filename + " :("
-        return None
+        return None, "sorry, no file existed with the name " + filename + " :("
 
 
 if __name__ == '__main__':
-    alice_in_wonderland = get_file("alice30.txt")
+    alice_in_wonderland, msg = get_file("alice30.txt")
+    # if file existed
     if alice_in_wonderland:
         # print alice_in_wonderland.read()
         alice_wc_dictionary = {}
@@ -22,3 +22,6 @@ if __name__ == '__main__':
                     else:
                         alice_wc_dictionary[formatted_word] = 0
         print alice_wc_dictionary
+    else:
+        # if file not existed
+        print msg
