7b64353b6dbd79165ebc096109e907d734c8d88c
Ron Shaiko <Ron.Shaiko@amdocs.com>
2018-02-11 15:57:35 +0200

    added dictionary and print values

--- wc.py
+++ wc.py
@@ -1,18 +1,24 @@
-
-
 def get_file(filename):
     try:
-        file = open(filename, "r") #opens file with name of "alice30.txt"
+        file = open(filename, "r")  # opens file with name of "alice30.txt"
         return file
     except Exception:
         print "sorry, no file existed with the name " + filename + " :("
         return None
 
+
 if __name__ == '__main__':
     alice_in_wonderland = get_file("alice30.txt")
     if alice_in_wonderland:
         # print alice_in_wonderland.read()
+        alice_wc_dictionary = {}
         with alice_in_wonderland as f:
             for line in f:
                 for word in line.split():
-                    print word.strip(',.:-;?`!').lower()
+                    formatted_word = word.strip(',.:-;?`!').lower()
+                    # print formatted_word
+                    if formatted_word in alice_wc_dictionary:
+                        alice_wc_dictionary[formatted_word] += 1
+                    else:
+                        alice_wc_dictionary[formatted_word] = 0
+        print alice_wc_dictionary
